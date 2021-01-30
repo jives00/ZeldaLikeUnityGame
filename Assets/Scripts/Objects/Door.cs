cf8f644b6bfe022c4f4172d4d56a46038efb2dda
jeff <jbromberek@gmail.com>
2021-01-30 08:04:09 -0600

    Leeson 36 - Key Doors + Misc Cleanups

---
+++ Assets/Scripts/Objects/Door.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DoorType {
+    key,
+    enemy,
+    button
+}
+
+public class Door : InteractableObjects {
+
+    [Header("Door Variables")]
+    public DoorType         thisDoorType;
+    public bool             open = false;
+    public Inventory        playerInventory;
+    public SpriteRenderer   doorSprite;
+    public BoxCollider2D    physicsCollider;
+
+
+    // if near door, has a key and spacebar pressed then call the Open() method
+    private void Update() {
+        playerInventory.numberOfKeys++;
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (playerInRange && thisDoorType == DoorType.key) {
+                if (playerInventory.numberOfKeys > 0) {
+                    playerInventory.numberOfKeys--;
+                    Open();}}}
+    }
+
+    // turn off sprite render, making it look like door is open.  turn off box collider
+    public void Open() {
+        doorSprite.enabled = false;
+        open = true;
+        physicsCollider.enabled = false;
+    }
+
+    public void Close() {
+
+    }
+}
