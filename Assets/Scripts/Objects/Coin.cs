93525efe3c9b1412440b7af0b9d1b6a21de832d9
jeff <jbromberek@gmail.com>
2021-01-31 12:39:36 -0600

    Lesson 39 - Added coins

---
+++ Assets/Scripts/Objects/Coin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : PowerUp {
+
+    public Inventory        playerInventory;
+
+    void Start() {
+        powerUpSignal.Raise();
+    }
+
+    public void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player") && !other.isTrigger) {
+            playerInventory.coins++;
+            powerUpSignal.Raise();
+            Destroy(this.gameObject);}
+    }
+}
