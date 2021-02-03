c94633337ed1d97cb0cd5fcc589c229419cec983
jeff <jbromberek@gmail.com>
2021-02-02 19:49:55 -0600

    Lesson 44 - Added switch controlled doors

---
+++ Assets/Scripts/Objects/Switch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+
+    public bool             active;
+    public BoolValue        storedValue;
+    public Sprite           activeSprite;
+    private SpriteRenderer  mySprite;
+    public Door             thisDoor;
+
+    void Start() {
+        mySprite    = GetComponent<SpriteRenderer>();
+        active      = storedValue.runTimeValue;
+        if (active) {
+            ActivateSwitch();}
+    }
+
+    public void ActivateSwitch() {
+        active = true;
+        storedValue.runTimeValue = active;
+        thisDoor.Open();
+        mySprite.sprite = activeSprite;      
+    }
+
+    public void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            ActivateSwitch();}
+    }
+
+
+}
