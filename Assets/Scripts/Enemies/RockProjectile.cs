1f9ac8eef0abd0681c6e230a727896125ac7834e
jeff <jbromberek@gmail.com>
2021-02-03 19:19:27 -0600

    Lesson 45 - Added Turret Enemy

---
+++ Assets/Scripts/Enemies/RockProjectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockProjectile : Projectile {
+
+    void Start() {
+                
+    }
+
+    void Update() {
+        
+    }
+}
---
+++ Assets/Scripts/Objects/Projectile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    [Header("Movement Variables")]
+    public float            moveSpeed;
+    public Vector2          directionToMove;
+    
+    [Header("Lifetime Variables")]
+    public float            lifetime;
+    
+    private float           lifetimeSeconds;
+    public Rigidbody2D     myRigidBody;
+
+    void Start() {
+        lifetimeSeconds = lifetime;
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        lifetimeSeconds -= Time.deltaTime;
+        if (lifetimeSeconds <= 0) {
+            Destroy(this.gameObject);}
+    }
+
+    public void Launch(Vector2 initialVelocity) {
+        myRigidBody.velocity = initialVelocity * moveSpeed;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other) {
+        Destroy(this.gameObject);
+    }
+}
